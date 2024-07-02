49417f764e7969caa38871f07a430e2c1c1f7982
bisd98 <dawidbis.98@gmail.com>
2024-07-02 21:34:31 +0200

    refactor(chunk): rewrite strategy pattern structure

--- retrieval_system/components/chunk.py
+++ retrieval_system/components/chunk.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from langchain.text_splitter import SpacyTextSplitter
 import spacy.cli
 from tqdm.auto import tqdm
@@ -6,44 +7,93 @@ import logging
 import warnings
 
 logging.disable()
-spacy.cli.download("en_core_web_lg")
 
-def spacy_text_splitter(text, pipeline="en_core_web_lg", chunk_size=512, chunk_overlap=64):
-    splitter = SpacyTextSplitter(
-        pipeline=pipeline, chunk_size=chunk_size, chunk_overlap=chunk_overlap
-    )
-    return splitter.split_text(text)
+try:
+    spacy.load("en_core_web_lg")
+except:
+    spacy.cli.download("en_core_web_lg")
 
+class TextSplitterStrategy(ABC):
+    @abstractmethod
+    def split_text(self, text):
+        """
+        Split the given text into chunks.
 
-def sentence_splitter(text, pipeline="en_core_web_lg", n_sentences=5):
-    nlp = spacy.load(pipeline)
-    doc = nlp(text)
-    sentences = [sent.text for sent in doc.sents]
-    
-    chunks = []
-    for i in range(0, len(sentences), n_sentences):
-        chunk = " ".join(sentences[i:i+n_sentences])
-        chunks.append(chunk)
-    
-    return chunks
+        Args:
+            text (str): The text to be split.
+
+        Returns:
+            list: A list of text chunks.
+        """
+        pass
+
+class SpacyTextSplitterStrategy(TextSplitterStrategy):
+    def __init__(self, pipeline="en_core_web_lg", chunk_size=512, chunk_overlap=64):
+        """
+        Initializes a SpacyTextSplitterStrategy object.
+
+        Args:
+            pipeline (str, optional): The name of the spaCy pipeline to use for text processing. Defaults to "en_core_web_lg".
+            chunk_size (int, optional): The maximum size of each chunk. Defaults to 512.
+            chunk_overlap (int, optional): The overlap size between chunks. Defaults to 64.
+        """
+        self.splitter = SpacyTextSplitter(
+            pipeline=pipeline, chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        )
+
+    def split_text(self, text):
+        """
+        Split the given text into chunks using SpacyTextSplitter.
+
+        Args:
+            text (str): The text to be split.
+
+        Returns:
+            list: A list of text chunks.
+        """
+        return self.splitter.split_text(text)
+
+class SentenceCountSplitterStrategy(TextSplitterStrategy):
+    def __init__(self, num_sentences=5):
+        """
+        Initializes a SentenceCountSplitterStrategy object.
+
+        Args:
+            num_sentences (int, optional): The number of sentences per chunk. Defaults to 5.
+        """
+        self.num_sentences = num_sentences
+
+    def split_text(self, text):
+        """
+        Split the given text into chunks based on the number of sentences.
+
+        Args:
+            text (str): The text to be split.
+
+        Returns:
+            list: A list of text chunks.
+        """
+        doc = spacy.load("en_core_web_lg")(text)
+        sentences = [sent.text for sent in doc.sents]
+        chunks = [
+            " ".join(sentences[i:i + self.num_sentences])
+            for i in range(0, len(sentences), self.num_sentences)
+        ]
+        return chunks
 
 class ArticleChunker:
-    def __init__(
-        self,
-        split_function=spacy_text_splitter,
-        pipeline="en_core_web_lg",
-        batch_size=256,
-    ):
+    def __init__(self, split_strategy=None, batch_size=128):
         """
         Initializes an ArticleChunker object.
 
         Args:
-            split_function (function): The function to use for text splitting.
-            pipeline (str, optional): The name of the spaCy pipeline to use for text processing. Defaults to "en_core_web_lg".
-            batch_size (int, optional): The number of samples to process in each batch. Defaults to 256.
+            split_strategy (TextSplitterStrategy, optional): The strategy to use for text splitting. 
+            If not provided, SpacyTextSplitterStrategy will be used as default.
+            batch_size (int, optional): The number of samples to process in each batch. Defaults to 128.
         """
-        self.split_function = split_function
-        self.nlp = spacy.load(pipeline)
+        if split_strategy is None:
+            split_strategy = SpacyTextSplitterStrategy()
+        self.split_strategy = split_strategy
         self.batch_size = batch_size
 
     def chunk_text(self, data):
@@ -71,7 +121,7 @@ class ArticleChunker:
             batch_data = data.iloc[batch_start:batch_end]
 
             for _, row in batch_data.iterrows():
-                chunks = self.splitter.split_text("".join(row.Text.split("\n\n")))
+                chunks = self.split_strategy.split_text("".join(row.Text.split("\n\n")))
                 for idx, chunk in enumerate(chunks):
                     documents.append(
                         {"title": row.Title, "chunk_idx": idx, "content": chunk}
